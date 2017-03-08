5a1728596a89bafab21655ce66d2c2bd0949de9e
Moaaz Nash <moaaznash@Moaazs-MacBook-Pro.local>
2017-03-08 11:30:40 +0200

    ssdf

---
+++ Mozeh.java
@@ -0,0 +1,15 @@
+package com.example.moaaznash.testata.mozeh;
+
+import android.os.DeadSystemException;
+
+/**
+ * Created by moaaznash on 3/8/17.
+ */
+
+public class Mozeh {
+    public void goo(){
+        System.out.println("Shit");
+
+
+    }
+}
